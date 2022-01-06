3987021d573d7639323b76714e63175ba1dd3838
Hampus Joakim Borgos <hey@borgos.se>
2022-01-06 17:22:40 +0100

    Use read_json

--- lib/heroku.sh
+++ lib/heroku.sh
@@ -2,7 +2,7 @@ run_prebuild() {
   local heroku_prebuild_script
   local build_dir="$1"
 
-  heroku_prebuild_script=$(json_get_key "$build_dir/package.json" ".scripts[\"heroku-prebuild\"]")
+  heroku_prebuild_script=$(read_json "$build_dir/package.json" ".scripts[\"heroku-prebuild\"]")
 
   if [[ $heroku_prebuild_script ]] ; then
     npm run heroku-prebuild
@@ -14,8 +14,8 @@ run_build() {
   local build_script
   local heroku_postbuild_script
 
-  build_script=$(json_get_key "$build_dir/package.json" ".scripts.build")
-  heroku_postbuild_script=$(json_get_key "$build_dir/package.json" ".scripts[\"heroku-postbuild\"]")
+  build_script=$(read_json "$build_dir/package.json" ".scripts.build")
+  heroku_postbuild_script=$(read_json "$build_dir/package.json" ".scripts[\"heroku-postbuild\"]")
 
   if [[ $heroku_postbuild_script ]] ; then
     npm run heroku-postbuild
